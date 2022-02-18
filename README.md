262fcac539022d68e7798274ecf59e56d2047f85
Šarūnas <srocevas@gmail.com>
2022-02-18 21:31:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Laravel Country seed
+
+Laravel country seed with phone codes.
+
+Migration with seed.
+
+### How to use
+
+Copy to the project root and run:
+
+    php artisan migrate --seed
---
+++ app/Models/Country.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code',
+        'name',
+        'phonecode'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+}
